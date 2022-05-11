ed1a56fb56198dcc318ca6956a2a59afdfaf3985
Riccardo Cuccia <riccardo.cuccia02@gmail.com>
2022-05-11 18:27:49 +0200

    feature: calculator

--- exercises/calculator.cpp
+++ exercises/calculator.cpp
@@ -10,3 +10,22 @@
   Multiplication: 8
   Division: 2
 */
+#include <iostream>
+using namespace std;
+void do_calculus(double n, double m){
+  cout << "SUM: " << n + m << endl;
+  cout << "Difference: " << n - m << endl;
+  cout << "Multiplication: " << n * m << endl;
+  cout << "Division: " << n / m << endl;
+  return;
+}
+
+int main(){
+  double n, m;
+  cout << "Insert first number: ";
+  cin >> n;
+  cout << endl << "Insert second number: ";
+  cin >> m;
+
+  do_calculus(n, m);
+}
